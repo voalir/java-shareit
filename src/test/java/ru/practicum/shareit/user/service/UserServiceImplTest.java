9bd4ad5c4202c2dcec6d47bd67ae5c7d91a0279a
Vladimir <voalir@yandex.ru>
2023-02-02 02:00:25 +0700

    add item request tests

--- src/test/java/ru/practicum/shareit/user/service/UserServiceImplTest.java
+++ src/test/java/ru/practicum/shareit/user/service/UserServiceImplTest.java
@@ -2,11 +2,11 @@ package ru.practicum.shareit.user.service;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.core.annotation.Order;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.test.context.jdbc.Sql;
 import ru.practicum.shareit.user.dto.UserDto;
@@ -17,7 +17,6 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-//@RunWith(SpringRunner.class)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @SpringBootTest
 class UserServiceImplTest {
